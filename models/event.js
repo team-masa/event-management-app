53e8762f6a27fdfb88531403e63f2f0745c87ea7
Sarah Acquaye Amedior <amediorsarah@gmail.com>
2024-07-02 12:09:37 +0000

    Event model

--- models/event.js
+++ models/event.js
@@ -1,2 +1,25 @@
-import { Model, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
+
+const eventSchema = new Schema ({
+    name:{type: String, required: true},
+    date:{type: Date, required: true},
+    price:{type: Number, required: true},
+    location:{type: String, required: true},
+    flierUrl:{type: String}
+});
+
+eventSchema.plugin(toJSON);
+export const EventModel = model('event', eventSchema);
+
+
+
+
+
+
+
+
+
+
+
+
